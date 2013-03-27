5010b5e1c7934a78b1f2a1dfcbe44183e7f93681
ian root <ianaroot@gmail.com>
2013-03-27 11:22:42 -0500

    Finish 10.3.2 Create microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -91,6 +91,23 @@ describe "Authentication" do
         end
         
       end
+
+      #<10.26>
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+        #</10.26>
+
+
     end
 
     describe "as wrong user" do
